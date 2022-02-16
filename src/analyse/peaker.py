0ad88de3019cead4239b759cef9f9026631ea7c4
Tim Tröndle <tim.troendle@usys.ethz.ch>
2022-02-16 13:49:02 +0100

    Update peaker plot to not show peaks beyond 2010

--- src/analyse/peaker.py
+++ src/analyse/peaker.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 import seaborn as sns
 
 from crisis import Crisis
@@ -8,6 +9,8 @@ from crisis import Crisis
 BLUE = '#4F6DB8'
 RED = '#A01914'
 GREY = "#7F7F7F"
+
+PEAK_YEAR_THRESHOLD = 2010 # We cannot identify peaks after 2010.
 
 
 def plot_peak_timeline(path_to_emissions, path_to_plot, crises, high_income, middle_income, path_to_peak_csv):
@@ -36,9 +39,9 @@ def plot_peak_timeline(path_to_emissions, path_to_plot, crises, high_income, mid
             alpha=0.2,
             color=GREY
         )
-    ax.bar(n_peaked_all.index, n_peaked_all.values, label="All with emission peaks", color=BLUE, alpha=0.4)
+    ax.bar(n_peaked_all.index, n_peaked_all.values, label="Medium-income with emission peaks", color=BLUE, alpha=0.4)
     ax.bar(n_peaked_high.index, n_peaked_high.values, label="High-income with emission peak", color=BLUE)
-    ax.bar(n_not_peaked_all.index, n_not_peaked_all.values, label="All without emission peak", color=RED, alpha=0.4)
+    ax.bar(n_not_peaked_all.index, n_not_peaked_all.values, label="Medium-income without emission peak", color=RED, alpha=0.4)
     ax.bar(n_not_peaked_high.index, n_not_peaked_high.values, label="High-income without emission peak", color=RED)
     ax.set_ylim(ax.get_ylim()[0], ax.get_ylim()[1] + 10)
     for crisis in crises:
@@ -56,6 +59,7 @@ def plot_peak_timeline(path_to_emissions, path_to_plot, crises, high_income, mid
     for t in abs_ticklabels:
         t.set_text(int(abs(t.get_position()[1])))
     ax.get_yaxis().set_ticklabels(abs_ticklabels)
+    ax.xaxis.set_minor_locator(mtick.MultipleLocator(1))
     fig.savefig(path_to_plot, dpi=300)
 
 
@@ -74,7 +78,7 @@ def cumsum_peaked(emissions):
     n_peaked.rename("Number of countries with emission peaks", inplace=True)
     n_peaked.index.rename("Year", inplace=True)
     n_not_peaked = -(emissions.notna().sum(axis=1) - n_peaked)
-    return n_peaked, n_not_peaked
+    return n_peaked.loc[:PEAK_YEAR_THRESHOLD], n_not_peaked.loc[:PEAK_YEAR_THRESHOLD]
 
 
 if __name__ == "__main__":
